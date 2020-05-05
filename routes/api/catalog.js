b450acd3cc8042aac9a43b92a8009cd72347f0d7
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-05 14:12:27 -0600

    Implemented RESTful get by id router for API

--- routes/api/catalog.js
+++ routes/api/catalog.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const {
-    getCatalog
+    getCatalog,
+    getTermByID
 } = require('../../dal/catalog');
 
 //Get Routers
@@ -14,5 +15,18 @@ router.get('/', async function(req, res){
         res.status(500).send('Internal server issue, check logs');
     }
 });
+router.get('/:id', async function(req, res) {
+    try{
+        const term = await getTermByID(id);
+        res.send(term);
+    }catch(err) {
+        if(err.error) {
+            res.status(400).send(err);
+        }else{
+            console.log(err);
+            res.status(500).send('Internal server issue, check logs');
+        }
+    }
+})
 
 module.exports = router;
