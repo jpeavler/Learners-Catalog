32dd80197ea1bdb6b8d9e7211ce99fb6883bdccc
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-05 14:23:15 -0600

    fixed get by id router bug

--- routes/api/catalog.js
+++ routes/api/catalog.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {
     getCatalog,
-    getTermByID
+    getTermByID,
+    getTermByName
 } = require('../../dal/catalog');
 
 //Get Routers
@@ -17,7 +18,7 @@ router.get('/', async function(req, res){
 });
 router.get('/:id', async function(req, res) {
     try{
-        const term = await getTermByID(id);
+        const term = await getTermByID(req.params.id);
         res.send(term);
     }catch(err) {
         if(err.error) {
@@ -27,6 +28,19 @@ router.get('/:id', async function(req, res) {
             res.status(500).send('Internal server issue, check logs');
         }
     }
-})
+});
+router.get('/name/:name', async function(req, res) {
+    try{
+        const term = await getTermByName(name);
+        res.send(term);
+    }catch(err) {
+        if(err.error) {
+            res.status(400).send(err);
+        }else{
+            console.log(err);
+            res.status(500).send('Internal server issue, check logs');
+        }
+    }
+});
 
 module.exports = router;
