aca8ab160e2db0e7a1728a89f743b72cd158c379
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-05 14:02:44 -0600

    Implemented RESTful get router for API

--- routes/api/catalog.js
+++ routes/api/catalog.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const {
+    getCatalog
+} = require('../../dal/catalog');
 
+//Get Routers
 router.get('/', async function(req, res){
-    
-})
+    try{
+        const catalog = await getCatalog();
+        res.send(catalog);
+    }catch(err) {
+        console.log(err);
+        res.status(500).send('Internal server issue, check logs');
+    }
+});
+
+module.exports = router;
