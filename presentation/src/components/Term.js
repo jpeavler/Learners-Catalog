bce1b0d323c6dd78fc8b5b6fa8ce16367c0f0846
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-06 08:46:50 -0600

    Implemented display of Catalog in UI

--- presentation/src/components/Term.js
+++ presentation/src/components/Term.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 const Term = ({term}) => {
+    const displayResources = term.resources.map((resource, index) => {
+        return(
+            <li key={index}><a href={resource.link} >{resource.displayName}</a></li>
+        )
+    })
     return (
-        <div className ='term'>
-            <h4>{term.name}</h4>
+        <div>
+            <h3>{term.name}</h3>
             <p>Definition: {term.definition}</p>
-            <p>Resources: {term.resources}</p>
-            
+            <h4>Resources</h4>
+            <ul>{displayResources}</ul>
         </div>
     )
 }
