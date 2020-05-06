12677d18e6d5cc0495da4cd316c853be5a92d23c
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-06 13:27:29 -0600

    adjusted display of Catalog

--- presentation/src/components/Term.js
+++ presentation/src/components/Term.js
@@ -3,14 +3,14 @@ import React from 'react';
 const Term = ({term}) => {
     const displayResources = term.resources.map((resource, index) => {
         return(
-            <li key={index}><a href={resource.link} >{resource.displayName}</a></li>
+            <li key={index}><a href={resource.link}>{resource.displayName}</a></li>
         )
     })
     return (
         <div>
             <h3>{term.name}</h3>
             <p>Definition: {term.definition}</p>
-            <h4>Resources</h4>
+            <h4>Resources for {term.name}</h4>
             <ul>{displayResources}</ul>
         </div>
     )
