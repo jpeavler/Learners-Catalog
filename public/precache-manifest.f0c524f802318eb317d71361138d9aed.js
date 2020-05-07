0ba4fe47ef6e9e69691769cb73399ef807f0e62e
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-07 17:16:18 -0600

    Preparing for deployment

---
+++ public/precache-manifest.f0c524f802318eb317d71361138d9aed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1c942bd806618bd0216f88897d41d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70191428d764b014ee38",
+    "url": "/static/css/main.b1ad1af9.chunk.css"
+  },
+  {
+    "revision": "b9e0aa7e6e3ff0be399b",
+    "url": "/static/js/2.d65d1738.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d65d1738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70191428d764b014ee38",
+    "url": "/static/js/main.212a76ca.chunk.js"
+  },
+  {
+    "revision": "325b1eb6ce020ede1a8a",
+    "url": "/static/js/runtime-main.71ab1c4e.js"
+  }
+]);
